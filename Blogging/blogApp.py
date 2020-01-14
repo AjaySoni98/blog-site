ad72d1fc664fddbb055ecd10439192beeb7879c4
Ajay Soni <ajay0312soni@gmail.com>
2020-01-14 22:50:03 +0530

    Add files via upload

--- Blogging/blogApp.py
+++ Blogging/blogApp.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, url_for, request, redirect
-
+from datetime import date
+from werkzeug.utils import secure_filename
 from flask_mysqldb import MySQL
 import yaml
+import os
+import tempfile
+
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
@@ -13,28 +20,12 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
 @app.route("/")
 @app.route("/home")
 def parallax():
     return render_template('parallax.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         # Fetch form data
@@ -44,11 +35,13 @@ def signup():
         username = userDetails['username']
         password = userDetails['password']
         confpassword = userDetails['confpassword']
+        profilepic = userDetails['profilepuc']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO user_signup(name, email, username, password, confpassword) VALUES(%s, %s, %s, %s, %s)",(name, email, username, password, confpassword))
+        cur.execute("INSERT INTO user_signup(name, email, username, password, profilepic) VALUES(%s, %s, %s, %s, %s)",(name, email, username, password, profilepic))
         mysql.connection.commit()
         cur.close()
         return render_template('parallax.html')
+        
     return render_template('parallax.html')
 
 
@@ -61,13 +54,11 @@ def login():
         password = userLoginDetails['password']
         cur = mysql.connection.cursor()
         cur.execute("SELECT * from user_signup where email='" + email + "' and password='" + password + "'")
-        
+        cur.execute("SELECT * FROM blog")
+        data = cur.fetchall()   
         mysql.connection.commit()
-        
-        data = cur.fetchone()
-        print(data[0])
         cur.close()
-        return render_template('login-home.html', userdata= posts)
+        return render_template('login-home.html', data=data)
         
 
     else:
@@ -75,9 +66,29 @@ def login():
 
 @app.route("/login-home")
 def home():
-    return render_template('login-home.html', posts=posts)
+    return render_template('login-home.html')
+
+@app.route("/post", methods=['POST'])
+def post():
+    if request.method == 'POST':
+        post = request.form
+        title = post['post-title']
+        descp = post['post-desc']
+        imag = request.files['post-image']
+        if imag:
+            imagname = secure_filename(imag.filename)
+            nam = tempfile.NamedTemporaryFile()
+            fname = nam.name[::-1]
+            imagname = fname[0:10]
+            imag.save(os.path.join(app.config['UPLOAD_FOLDER'], imagname))
+        image = "static/images/"+imagname
+        today = date.today()
+        datee = today.strftime("%d %B, %Y")
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO blog(title, description, image, date) VALUES(%s, %s, %s, %s)",(title, descp, image, datee))
+        mysql.connection.commit()
+        cur.close()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
--- Blogging/static/main.js
+++ Blogging/static/main.js
@@ -7,3 +7,16 @@ function signupFunc(){
     document.getElementById("signup-form").style.visibility = "visible";
     document.getElementById("login-form").style.visibility = "hidden";
 }
+
+function newPost(){
+    document.getElementById("post").classList.toggle("postbox1");
+    document.getElementById("blur").style.filter = "blur(1px)";
+    document.getElementById("blur1").style.filter = "blur(1px)";
+    document.getElementById("blur2").style.filter = "blur(1px)";
+}
+
+function deblur(){
+    document.getElementById("blur").style.filter = "blur(0px)";
+    document.getElementById("blur1").style.filter = "blur(0px)";
+    document.getElementById("blur2").style.filter = "blur(0px)";
+}
